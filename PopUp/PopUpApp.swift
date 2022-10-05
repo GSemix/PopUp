a63e9e2f1188b8ac67d59115d0004aeadbb77d26
GSemix <63607553+GSemix@users.noreply.github.com>
2022-10-05 17:02:47 +0300

    Initial Commit

---
+++ PopUp/PopUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopUpApp.swift
+//  PopUp
+//
+//  Created by Семен Безгин on 05.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct PopUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
